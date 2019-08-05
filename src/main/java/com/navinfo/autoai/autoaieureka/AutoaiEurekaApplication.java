2bd0820a2dd81d286d049894375576d5d0405423
yanghh <136158344@qq.com>
2019-08-05 15:10:44 +0800

    update 1

---
+++ src/main/java/com/navinfo/autoai/autoaieureka/AutoaiEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.navinfo.autoai.autoaieureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoaiEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoaiEurekaApplication.class, args);
+    }
+
+}
